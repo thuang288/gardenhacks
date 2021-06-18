a7f7ab0ebe333c60ad0a637936aa7d47bedc368d
Tommy <thuang20017@gmail.com>
2021-06-18 19:42:21 -0400

    first

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { Suspense } from "react";
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import { Html, useGLTF } from "@react-three/drei";
+import Globe from 'react-globe.gl';
+
+// const Model = () => {
+//   const gltf = useGLTF("/earth.gltf", true);
+//   return <primitive object={gltf.scene} dispose={null} />;
+// };
+
+// const HTMLContent = () => {
+//   return (
+//     <group position={[0, 250, 0]}>
+//       <Html fullscreen>
+//         <mesh position={[0,35,0]}>
+//           <Model />
+//         </mesh>
+//       </Html>
+//     </group>
+//   );
+// };
+
+  const Earth = () => {
+    return (
+      <>
+      <Globe />
+      </>
+    )
+  }
+
+  const Lights = () => {
+    return (
+      <ambientLight intensity={.3}/>
+    )
+  }
+
+function App() {
+  return (
+    <>
+      {/* <header className="App-header">
+      </header> */}
+      {/* <Canvas colorManagement camera={{ position: [0, 0, 120], fov: 50 }}>
+        <Suspense fallback={null}>
+          <HTMLContent />
+        </Suspense>
+      </Canvas> */}
+      <Earth
+        globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+      />
+    </>
+  );
+}
+
+export default App;
