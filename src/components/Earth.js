7157530d89946f531755ab5d0cb54631e3aca9b5
Tommy <thuang20017@gmail.com>
2021-06-19 01:53:33 -0400

    added rotating effect

--- src/components/Earth.js
+++ src/components/Earth.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Globe from 'react-globe.gl';
 
 export default function Earth() {
+    
+    const globeEl = useRef();
+    
+    useEffect(() => {
+        // Auto-rotate
+        globeEl.current.controls().autoRotate = true;
+        globeEl.current.controls().autoRotateSpeed = 0.3;
+    }, []);
+
     return (
     <Globe 
+        ref={globeEl}
         backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
         globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg" 
     />
