fbf30a4bc2ddbd8bbb5cb21e910703c3f453a324
Tommy <thuang20017@gmail.com>
2021-06-19 02:20:49 -0400

    added comments

--- src/components/Earth.js
+++ src/components/Earth.js
@@ -5,6 +5,22 @@ export default function Earth() {
     
     const globeEl = useRef();
    
+    // i think this is where the api goes?? not sure 
+
+    // useEffect(() => {
+    //     // load data
+    //     fetch('../datasets/ne_110m_admin_0_countries.geojson').then(res => res.json())
+    //       .then(countries=> {
+    //         setCountries(countries);
+
+    //  this part is to show the effects like this https://github.com/vasturiano/react-globe.gl/blob/master/example/countries-population/preview.png
+    
+    //         setTimeout(() => {
+    //           setTransitionDuration(4000);
+    //           setAltitude(() => feat => Math.max(0.1, Math.sqrt(+feat.properties.POP_EST) * 7e-5));
+    //         }, 3000);
+    //       });
+    //   }, []);
  
     useEffect(() => {
         // Auto-rotate
